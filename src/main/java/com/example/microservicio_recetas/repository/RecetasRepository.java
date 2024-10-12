245a3c0137123fe6a7491b06b9b5872325eed716
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-10-12 18:34:56 -0400

    [Fix] cambiando de tablas pacientes y medicos a usuarios

--- src/main/java/com/example/microservicio_recetas/repository/RecetasRepository.java
+++ src/main/java/com/example/microservicio_recetas/repository/RecetasRepository.java
@@ -13,18 +13,18 @@ public interface RecetasRepository extends JpaRepository<RecetasEntity, Integer>
     @Query(value="SELECT id_receta, nombre_generico_medicamento_preescrito, via_cuidado_especiales_administracion, concentracion_dosificacion, frecuencia_administracion_24hrs, duracion_tratamiento, fecha_vencimiento, precauciones_especiales, indicaciones_especiales, r.id_medico, hc.id_historia_clinica, r.created_at, r.updated_at,p.ci,CONCAT(p.nombres,p.apellido_paterno,' ',p.apellido_materno),r.deleted_at "+
     "FROM recetas r  "+
     "INNER JOIN historias_clinicas hc ON hc.id_historia_clinica = r.id_historia_clinica "+
-    "INNER JOIN pacientes p ON hc.id_paciente = p.id_paciente",nativeQuery=true)
+    "INNER JOIN usuarios p ON hc.id_paciente = p.id_usuario",nativeQuery=true)
     List<Object[]> obtenerRecetas();
     @Query(value="SELECT id_receta, nombre_generico_medicamento_preescrito, via_cuidado_especiales_administracion, concentracion_dosificacion, frecuencia_administracion_24hrs, duracion_tratamiento, fecha_vencimiento, precauciones_especiales, indicaciones_especiales, r.id_medico, hc.id_historia_clinica, r.created_at, r.updated_at,p.ci,CONCAT(p.nombres,p.apellido_paterno,' ',p.apellido_materno),r.deleted_at "+
     "FROM recetas r  "+
     "INNER JOIN historias_clinicas hc ON hc.id_historia_clinica = r.id_historia_clinica "+
-    "INNER JOIN pacientes p ON hc.id_paciente = p.id_paciente "+
-    "WHERE p.id_paciente=?1",nativeQuery=true)
+    "INNER JOIN usuarios p ON hc.id_paciente = p.id_usuario "+
+    "WHERE p.id_usuario=?1",nativeQuery=true)
     List<Object[]> obtenerRecetasPaciente(int idPaciente);
     @Query(value="SELECT id_receta, nombre_generico_medicamento_preescrito, via_cuidado_especiales_administracion, concentracion_dosificacion, frecuencia_administracion_24hrs, duracion_tratamiento, fecha_vencimiento, precauciones_especiales, indicaciones_especiales, r.id_medico, hc.id_historia_clinica, r.created_at, r.updated_at,p.ci,CONCAT(p.nombres,p.apellido_paterno,' ',p.apellido_materno),r.deleted_at "+
     "FROM recetas r  "+
     "INNER JOIN historias_clinicas hc ON hc.id_historia_clinica = r.id_historia_clinica "+
-    "INNER JOIN pacientes p ON hc.id_paciente = p.id_paciente "+
+    "INNER JOIN usuarios p ON hc.id_paciente = p.id_usuario "+
     "WHERE r.id_receta=?1",nativeQuery=true)
     List<Object[]> obtenerRecetaPorId(int id);
 }
